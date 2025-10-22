9549343f099ac799c03c636dd1a56d22293aae12
TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
2025-10-22 13:36:06 -0600

    More Organizations

--- source/test_files/blockds_examples.h
+++ source/test_files/blockds_examples.h
@@ -21,7 +21,7 @@
 #include "../graphics/test_graphics/tiny_16.h"
 #include "../graphics/test_graphics/advnt.h"
 
-#include "../tileset.h"
+#include "../components/tileset.h"
 
 #define MAP_WIDTH 30
 #define MAP_HEIGHT 20
