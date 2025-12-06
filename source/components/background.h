7d1ab82a2ec40c8b2955e7651702ac8c6c98e48d
TheGreatRy <158355076+TheGreatRy@users.noreply.github.com>
2025-12-05 19:07:10 -0700

    removed grits

--- source/components/background.h
+++ source/components/background.h
@@ -9,12 +9,11 @@ class Background
 public:
 
 
-    Background(int layer, BgType bg_type, BgSize bg_size, int map_base, int tile_base, bool is_main);
+    Background();
     ~Background() = default;
 
     int m_background_id;
     bool m_is_main;
 
-    void LoadBackground(int bitmap_base, const unsigned int* bitmap_source, uint32_t bitmap_size, const unsigned short*  pal_source, uint32_t pal_size);
-    void LoadBackground(int bitmap_base, const unsigned int* bitmap_source, uint32_t bitmap_size);
+    
 };
